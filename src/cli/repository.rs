ea3a8ec8d41ed5dc3a737c4ee432e201ffd92a00
Manel Caireta <m.caireta@altostratus.es>
2024-09-20 20:44:34 +0200

    test: update repository tests

--- src/cli/repository.rs
+++ src/cli/repository.rs
@@ -3,7 +3,8 @@ use super::version::PasswordVersion;
 use std;
 use std::fs::{self, create_dir, read_dir, File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
-use std::path::PathBuf;
+use std::os::unix::ffi::OsStrExt;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
 pub struct PasswordRepository {
@@ -55,36 +56,44 @@ impl PasswordRepository {
             .expect("Couldn't save password");
     }
 
-    pub fn get(&self, password_name: &str) -> Result<PasswordVersion, Box<dyn std::error::Error>> {
-        let mut file_path = self.root_dir.clone();
-        file_path.push(password_name);
+    pub fn get(
+        &self,
+        password_name: &str,
+    ) -> Result<PasswordVersion, Box<dyn std::error::Error>> {
+        self.get_latest_version(password_name)
+    }
 
-        let current_version: u32;
+    pub fn get_latest_version(
+        &self,
+        password_name: &str,
+    ) -> Result<PasswordVersion, Box<dyn std::error::Error>> {
+        let mut password_versions_path =
+            self.root_dir.join(Path::new(password_name));
 
-        let read_dir = match read_dir(file_path) {
-            Ok(result) => result,
-            Err(_) => {
-                println!("The password {} does not exist", password_name);
-                exit(1)
+        let mut current_version: u32;
+
+        for entry in read_dir(password_versions_path)? {
+            let entry = entry?;
+            let version = match entry.file_name().into_string() {
+                Ok(version) => version.parse::<u32>()?,
+                Err(_) => {
+                    eprintln!("pwm: Corrupted file detected in password {password_name} folder");
+                    continue;
+                }
+            };
+
+            if current_version < version {
+                current_version = version;
             }
-        };
+        }
 
-        let file = match File::open(file_path) {
-            Ok(file) => file,
-            Err(_) => return None,
-        };
+        let password_path = password_versions_path
+            .join(Path::new(current_version.to_string().as_str()));
 
-        let mut reader = BufReader::new(file);
-        let mut password_string = String::new();
-
-        reader
-            .read_line(&mut password_string)
-            .expect("Error reading password");
-
-        Some(Password::new(
-            password_name.to_string(),
-            password_string.trim().to_string(),
-        ))
+        let password_value = fs::read_to_string(password_path)?;
+        let password =
+            Password::new(password_name.to_string(), password_value);
+        Ok(PasswordVersion::new(password, current_version))
     }
 
     pub fn list(&self) {
@@ -128,13 +137,14 @@ mod tests {
     fn save_and_update_new_password() {
         let password =
             Password::new("test-password".to_string(), "TEST".to_string());
+        let password_version = PasswordVersion::new(password, 1);
         let password_repo = PasswordRepository::new();
 
         password_repo.remove(&password.name());
         password_repo.add(&password);
 
         assert_eq!(
-            password,
+            password_version,
             password_repo
                 .get(password.name())
                 .expect("Couldn't get value")
@@ -143,9 +153,10 @@ mod tests {
         let new_password =
             Password::new(password.name().to_string(), "NEW-TEST".to_string());
         password_repo.update(&new_password);
+        let new_password_version = PasswordVersion::new(new_password, 1);
 
         assert_eq!(
-            new_password,
+            new_password_version,
             password_repo
                 .get(&new_password.name())
                 .expect("Couldn't get value")
